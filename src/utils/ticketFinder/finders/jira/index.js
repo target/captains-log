76b96cd353ee5d85c478c100f8723768903f8933
Anders D. Johnson <615381+AndersDJohnson@users.noreply.github.com>
2018-10-11 15:53:17 -0500

    fix: update Jira issue regex to no leading number

--- src/utils/ticketFinder/finders/jira/index.js
+++ src/utils/ticketFinder/finders/jira/index.js
@@ -3,7 +3,7 @@ const nconf = require('nconf');
 
 const { groupFinder } = require('../../../');
 
-const regex = nconf.get('regex') || /(?:\[|https:\/\/jira\..*\.com\/browse\/)([A-Z0-9]+-[0-9]+)\]?/;
+const regex = nconf.get('regex') || /(?:\[|https:\/\/jira\..*\.com\/browse\/)([[A-Z][A-Z0-9]*-[1-9][0-9]*)\]?/;
 const JIRA_REGEX = new RegExp(regex, 'g');
 
 const jiraFinder = (body) => {
