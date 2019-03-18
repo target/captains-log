0f52211603afe69e53d60472b0b2170616e3efa2
therynamo <groetkentheryn@gmail.com>
2019-03-18 16:43:41 -0500

    Introduce Github V4 Api For Chronological Tags

--- src/handlers/getTagsHandler.js
+++ src/handlers/getTagsHandler.js
@@ -1,13 +1,46 @@
-const { Github: github } = require('../connectors');
-const logger = require('../logger');
+const idx = require('idx');
+
+const { Github } = require('../connectors');
+
+const github = Github(true);
 
 module.exports = async function getTags(owner, repo) {
-  let tags = [];
+  let repository = [];
   try {
-    tags = await github.repos.getTags({ owner, repo });
+    console.log(Github);
+    console.log(github);
+    console.log(github);
+    console.log(github);
+    console.log(github);
+    repository = await github({
+      query: `query GetTags($owner: String!, $repo: String!) {
+        repository(owner: $owner, name: $repo) {
+          refs(
+            refPrefix: "refs/tags/"
+            first: 100
+            orderBy: { field: TAG_COMMIT_DATE, direction: DESC }
+          ) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }`,
+      owner,
+      repo,
+    });
   } catch (e) {
     throw e;
   }
 
-  return tags.data;
+  const { repository: rep } = repository;
+  const response = idx(rep, _ => _.refs.edges) || [];
+
+  // We need to put the tags back into their original format so that we can be backwards compatible with
+  // the expected v3 response.
+  const tags = response.reduce((acc, edge) => [...acc, { name: idx(edge, _ => _.node.name) || '' }], []);
+
+  return tags;
 };
