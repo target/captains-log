0cd9cc9b092920470ee0cb5f32eb399f25b197ad
Theryn Groetken <therynamo@users.noreply.github.com>
2019-05-03 09:02:19 -0500

    Add Org/Repo to Default Heading (#34)
    closes #32
    closes #33

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,15 @@
 
 const { stripIndents } = require('common-tags');
 
-const DEFAULT_HEADING = `🚀 *Deployed to production!* Related stories:\n _PRs need correct syntax to appear in the list. Problem? Please report it <https://github.com/target/captains-log/issues|here>._
+const DEFAULT_HEADING = (
+  strings,
+  owner,
+  repo,
+) => `🚀 *${owner}/${repo} deployed to production!* Related stories:\n _PRs need correct syntax to appear in the list. Problem? Please report it <https://github.com/target/captains-log/issues|here>._
 `;
 
 const EMPTY_MESSAGE = stripIndents`\n
-  *No Jira stories linked in this release.* 🤷‍♀️ \n
+  *No stories linked in this release.* 🤷‍♀️ \n
 
 _Think something is broken? Report it <https://github.com/target/captains-log/issues|here>._
 `;
