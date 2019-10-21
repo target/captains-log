b2b3ffe008847167c603e3a7f8ac3a461f73d267
Ben Zvan <ben@zvan.net>
2019-10-21 12:43:35 -0500

    Consistent appearance of slack posts (#44)
    * Consistent appearance of slack posts
    
    The post will have a consistent look regardless of
    if we could find any references to jira stories.
    
    * Make the changes only in constants
    
    * more javascripty

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,17 +2,17 @@
 
 const { stripIndents } = require('common-tags');
 
-const DEFAULT_HEADING = (
-  owner,
-  repo,
-) => `🚀 *${owner}/${repo} deployed to production!* Related stories:\n _PRs need correct syntax to appear in the list. Problem? Please report it <https://github.com/target/captains-log/issues|here>._
-`;
+const DEFAULT_FOOTER = stripIndents
+`_PRs need correct syntax to appear in the list. Problem? Report it <https://github.com/target/captains-log/issues|here>._`;
 
-const EMPTY_MESSAGE = (owner, repo) => stripIndents`\n
-  *No stories linked in this release for ${owner}/${repo}.* 🤷‍♀️ \n
+const DEFAULT_HEADING = ( owner, repo,) => 
+`🚀 *${owner}/${repo} deployed to production!* Related stories:\n
+${DEFAULT_FOOTER}`;
 
-_Think something is broken? Report it <https://github.com/target/captains-log/issues|here>._
-`;
+const EMPTY_MESSAGE = (owner, repo) => stripIndents
+`🚀 *${owner}/${repo} deployed to production!*\n
+  *No stories linked in this release* 🤷‍♀️ \n
+  ${DEFAULT_FOOTER}`;
 
 module.exports = {
   DEFAULT_HEADING,
