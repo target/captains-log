1c4d54181ea8b941b02b65fefbb2b67cf1257e46
Ben Zvan <ben.zvan@target.com>
2019-10-21 09:42:41 -0500

    more javascripty

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,11 +6,13 @@ const DEFAULT_FOOTER = stripIndents
 `_PRs need correct syntax to appear in the list. Problem? Report it <https://github.com/target/captains-log/issues|here>._`;
 
 const DEFAULT_HEADING = ( owner, repo,) => 
-`🚀 *${owner}/${repo} deployed to production!* Related stories:\n` + DEFAULT_FOOTER;
+`🚀 *${owner}/${repo} deployed to production!* Related stories:\n
+${DEFAULT_FOOTER}`;
 
 const EMPTY_MESSAGE = (owner, repo) => stripIndents
 `🚀 *${owner}/${repo} deployed to production!*\n
-  *No stories linked in this release* 🤷‍♀️ \n` + DEFAULT_FOOTER ;
+  *No stories linked in this release* 🤷‍♀️ \n
+  ${DEFAULT_FOOTER}`;
 
 module.exports = {
   DEFAULT_HEADING,
