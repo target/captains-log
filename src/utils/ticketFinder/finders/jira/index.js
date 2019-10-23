4ba05d98871e7ea1b9facf2bc7e267e259e815e6
therynamo <groetkentheryn@gmail.com>
2019-10-23 07:55:51 -0500

    Jira Branch Name Convetion Support

--- src/utils/ticketFinder/finders/jira/index.js
+++ src/utils/ticketFinder/finders/jira/index.js
@@ -1,15 +1,24 @@
+const idx = require('idx');
 const { uniq } = require('lodash');
 const nconf = require('nconf');
 
 const { groupFinder } = require('../../../');
 
-const regex = nconf.get('regex') || /(?:\[|https:\/\/jira\..*\.com\/browse\/)([[A-Z][A-Z0-9]*-[1-9][0-9]*)\]?/;
-const JIRA_REGEX = new RegExp(regex, 'g');
+const bodyRegex = nconf.get('regex') || /(?:\[|https:\/\/jira\..*\.com\/browse\/)([[A-Z][A-Z0-9]*-[1-9][0-9]*)\]?/;
+const branchRegex = /([[A-Z][A-Z0-9]*-[1-9][0-9]*)\]?/;
 
-const jiraFinder = (body) => {
+const JIRA_REGEX = new RegExp(bodyRegex, 'g');
+const JIRA_BRANCH_REGEX = new RegExp(branchRegex, 'g');
+
+const jiraFinder = (pr) => {
+  const { body = '' } = pr;
+
   const jiraTickets = uniq(groupFinder(JIRA_REGEX, body) || []);
 
-  const formattedTickets = jiraTickets.map(ticket => ({
+  const branchName = idx(pr, _ => _.head.ref);
+  const branchTicket = uniq(groupFinder(JIRA_BRANCH_REGEX, branchName) || []);
+
+  const formattedTickets = [...jiraTickets, ...branchTicket].map(ticket => ({
     name: ticket,
   }));
 
