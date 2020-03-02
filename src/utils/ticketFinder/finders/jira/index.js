3908059ed839a67c829626a3979f8a227ea8513f
therynamo <groetkentheryn@gmail.com>
2020-03-02 10:20:32 -0600

    fix: Jira Branch Regex

--- src/utils/ticketFinder/finders/jira/index.js
+++ src/utils/ticketFinder/finders/jira/index.js
@@ -5,12 +5,12 @@ const nconf = require('nconf');
 const { groupFinder } = require('../../../');
 
 const bodyRegex = nconf.get('regex') || /(?:\[|https:\/\/jira\..*\.com\/browse\/)([[A-Z][A-Z0-9]*-[1-9][0-9]*)\]?/;
-const branchRegex = /([[A-Z][A-Z0-9]*-[1-9][0-9]*)/;
+const branchRegex = /([A-Z][A-Z0-9]*-[1-9][0-9]*)/;
 
 const JIRA_REGEX = new RegExp(bodyRegex, 'g');
 const JIRA_BRANCH_REGEX = new RegExp(branchRegex, 'g');
 
-const jiraFinder = (pr) => {
+const jiraFinder = pr => {
   const { body = '' } = pr;
 
   const jiraTickets = uniq(groupFinder(JIRA_REGEX, body) || []);
