9a95d35857d352291f664fb7368cc8416f335c71
Theryn Groetken <therynamo@users.noreply.github.com>
2020-03-27 11:11:14 -0500

    fix: Global Regex Not Correctly Finding Tags (#66)

--- src/utils/__tests__/getTagDiffFromTagId.test.js
+++ src/utils/__tests__/getTagDiffFromTagId.test.js
@@ -15,4 +15,10 @@ describe('getTagDiffFromTagId', () => {
     expect(head).toEqual('v301-release');
     expect(base).toEqual('v300-release');
   });
+  it('should retrieve the first two tags based on the regexp passed even if in numerical order', () => {
+    const { head, base } = getTagDiffFromTagId('([0-9])', [{ name: '201' }, { name: '200' }]);
+
+    expect(head).toEqual('201');
+    expect(base).toEqual('200');
+  });
 });
